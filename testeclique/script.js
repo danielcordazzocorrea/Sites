feb57e6018fd04d4f5694c0c55b6f60b0fc9de68
Daniel Cordazzo Correa <danielcordazzocorrea@gmail.com>
2025-03-05 09:42:47 -0300

    comecei

--- testeclique/script.js
+++ testeclique/script.js
@@ -1,5 +1,7 @@
 function clicar(){
+    mensagem.innerHTML = ''
     if (botao.innerHTML == 'Inicie'){
+    qcliques.innerHTML = 0
     botao.innerHTML = 'Pare'
     let cont = 0
     intervalo = setInterval(() =>{
@@ -8,11 +10,11 @@ function clicar(){
     if (cont == 5){
     clearInterval(intervalo)
     botao.innerHTML = 'Inicie'
-    qcliques.innerHTML = 0
     contador.innerHTML = 0
 }
 }, 1000)
     }else{
+        contador.innerHTML = 0
         botao.innerHTML = 'Inicie'
         clearInterval(intervalo)
     }
@@ -23,6 +25,9 @@ function contar(){
     if (botao.innerHTML == 'Pare'){
         cliques = Number(qcliques.innerHTML)
         cliques += 1
-        qcliques.innerHTML = cliques 
+        qcliques.innerHTML = cliques
+        let quantidade_cliques = cliques 
+    }else{
+        mensagem.innerHTML = 'Aperte para iniciar primeiro'
     }
 }
