5a48c9924ccf4acb57597b5814c9f0858e743369
Daniel Cordazzo Correa <danielcordazzocorrea@gmail.com>
2025-03-05 14:18:01 -0300

    atualizei

--- testeclique/script.js
+++ testeclique/script.js
@@ -20,17 +20,24 @@ function clicar(){
             imagens.innerHTML = '<img id="res_imagem" src="imagens/cara bombado.avif" alt=""></img>'
             if (quantidade_cliques >= 1 && quantidade_cliques <= 10){
                 res.innerHTML = 'Tu tá mal em'
+                imagens.innerHTML = '<img id="res_imagem" src="imagens/Tu é lento.webp" alt=""></img>'
             }else if(quantidade_cliques > 10 && quantidade_cliques <= 20){
                 res.innerHTML = 'Tá lentinho'
+                imagens.innerHTML = '<img id="res_imagem" src="imagens/lento.png" alt=""></img>'
             }else if(quantidade_cliques > 20 && quantidade_cliques <= 30){
                 res.innerHTML = 'Ta normal ai'
+                imagens.innerHTML = '<img id="res_imagem" src="imagens/pessoa normal.jpg" alt=""></img>'
             }else if(quantidade_cliques > 30 && quantidade_cliques < 40){
                 res.innerHTML = 'Ta legalzin'
+                imagens.innerHTML = '<img id="res_imagem" src="imagens/feliz.jpg" alt=""></img>'
             }else if(quantidade_cliques >= 40 && quantidade_cliques < 60){
                 res.innerHTML = 'Po maluco ta bom d+ da conta'
+                imagens.innerHTML = '<img id="res_imagem" src="imagens/cara bombado.avif" alt=""></img>'
             }else if(quantidade_cliques >= 60){
                 res.innerHTML = 'HACKER'
+                imagens.innerHTML = '<img id="res_imagem" src="imagens/hacker.webp" alt=""></img>'
             }else{
+                imagens.innerHTML = '<img id="res_imagem" src="imagens/sem clicar.avif" alt=""></img>'
                 res.innerHTML = 'CLICA AI MEU'
                 res.style.color = 'red'
             }
@@ -52,5 +59,6 @@ function tentar(){
         tentar_novamente.innerHTML = ''
         res.innerHTML = ''
         tentar_novamente.className = ''
+        imagens.innerHTML = ''
     }
 }
