55f3885b509d684499f5d2211a30b1d982e0b213
Daniel Cordazzo Correa <danielcordazzocorrea@gmail.com>
2025-03-17 18:48:20 -0300

    terminei

---
+++ testereflexo/script.js
@@ -0,0 +1,9 @@
+// Função para gerar número aleatório inteiro entre min e max
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+console.log(Math.random())
+function clicar() {
+    
+}
